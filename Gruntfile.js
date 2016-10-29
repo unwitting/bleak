6ab1171dd7e26835974b399210fffb8b72a6311c
Jack Preston <unwttng@gmail.com>
2016-10-29 11:41:00 +0100

    AMPlifying, closes #23

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-sass-lint');
   grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-replace');
 
   grunt.initConfig({
 
@@ -64,12 +67,30 @@ module.exports = function(grunt) {
           interrupt: true
         }
       }
+    },
+
+    replace: {
+      amppostcss: {
+        options: {
+          patterns: [
+            {
+              match: 'compiled_css',
+              replacement: function () {
+                return fs.readFileSync('./assets/compiled/css/post.css');
+              }
+            }
+          ]
+        },
+        files: [
+          {expand: true, flatten: true, src: ['partials/amp_style.hbs'], dest: 'partials/compiled'}
+        ]
+      }
     }
 
   });
 
-  grunt.registerTask('css-dev', ['sass:dev']);
-  grunt.registerTask('css-prod', ['sass:prod', 'postcss:compiled']);
+  grunt.registerTask('css-dev', ['sass:dev', 'replace:amppostcss']);
+  grunt.registerTask('css-prod', ['sass:prod', 'postcss:compiled', 'replace:amppostcss']);
 
   grunt.registerTask('lint-sass', ['sasslint']);
   grunt.registerTask('lint', ['lint-sass']);
